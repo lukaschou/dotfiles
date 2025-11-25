64427e460b84e8ddca3aa62f9639b8b9f3ae7f57
Lukas Chou <lukaschou06@outlook.com>
2025-11-25 03:07:06 +0000

    NVIM keymaps

--- dot-config/nvim/lua/config/keymap.lua
+++ dot-config/nvim/lua/config/keymap.lua
@@ -1,6 +1,18 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pl", function()
+    vim.opt.relativenumber = not(vim.opt.relativenumber:get())
+end)
+vim.keymap.set("n", "<leader>nj", vim.cmd.split)
+vim.keymap.set("n", "<leader>nl", vim.cmd.vs)
+vim.keymap.set("n", "<leader>w<bs>", vim.cmd.wq)
+vim.keymap.set("n", "<leader>wl", "<c-w>l")
+vim.keymap.set("n", "<leader>wr", "<c-w>r")
+vim.keymap.set("n", "<leader>wj", "<c-w>j")
+vim.keymap.set("n", "<leader>wk", "<c-w>k")
+vim.keymap.set("n", "<leader>ws", vim.cmd.w)
+
 vim.keymap.set("n", "<leader>ps", function()
     vim.cmd("w")
     vim.cmd("so")
